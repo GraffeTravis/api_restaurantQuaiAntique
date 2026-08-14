fcb43959dc81c1db18e4db59a4a6eacfe50d63f5
Travis <contact.travisgraffe@gmail.com>
2026-08-14 10:32:09 -1000

    Route created and Controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,13 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class DefaultController extends AbstractController
 {
     #[Route('/')]
-    public function home() : Response
+    public function home(): Response
     {
-	return new Response("Bienvenue sur votre accueil !");
-     }
+        return new Response('Bienvenue sur votre accueil !');
+    }
 }
