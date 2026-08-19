226e1fe8885724dbfaceab1a003a0aeb541bc3f7
Travis <contact.travisgraffe@gmail.com>
2026-08-18 14:08:48 -1000

    Uses added to ApiTOken

--- src/Security/ApiTokenAuthenticator.php
+++ src/Security/ApiTokenAuthenticator.php
@@ -2,12 +2,16 @@
 
 namespace App\Security;
 
-use src\Repository\UserRepository;
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\{JsonResponse, Request, Response};
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 
 
 class ApiTokenAuthenticator extends AbstractAuthenticator
